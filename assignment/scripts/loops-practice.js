661da6aca2582fd0a493c1374298ed69c4b1409f
Heather Haymond <hheatherjean11@gmail.com>
2024-03-07 23:43:27 -0600

    getting better at my work flow and commiting ofetn

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -19,14 +19,20 @@ for (let i=0; i<6; i++) { // account for the count starting at 0
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-
+for (let i=3; i<6; i++) { // the count starting at 3 but needs the extra increment 
+  console.log(i); // expect 3,4,5
+}
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);//expect 2,4,6,8,10
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i = 5; i >= 0; i--) {
+  console.log(i);//expect 5,4,3,2,1,0
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -34,6 +40,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+for (const star of stars) { //iterates over each element of the stars array. each time assigning the current star to the variable star.
+  console.log(star); // each iteration is saved in this loop. expect 'Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'
+}
 
 
 // 3. While loops  
